75c47205f78b04d0d1c00536f038d90e74fe1a19
anton_remezovskyi2508 <antonremezovskyi@gmail.com>
2024-11-23 22:33:17 +0200

    finish with add types for project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, FC } from "react";
 import { Toaster, toast } from "react-hot-toast";
 
 import ImageGallery from "../ImageGallery/ImageGallery";
@@ -11,13 +11,14 @@ import ImageModal from "../ImageModal/ImageModal";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn";
+import { ImageType, ImageInfo, ApiResponse } from "./Type";
 
-function App() {
+const App: FC = () => {
   //Modal open/close;
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const [selectedImg, setSelectedImg] = useState("");
+  const [modalIsOpen, setIsOpen] = useState<boolean>(false);
+  const [selectedImg, setSelectedImg] = useState<ImageType | null>(null);
 
-  const openModal = (image) => {
+  const openModal = (image: ImageInfo) => {
     setSelectedImg({
       url: image.urls.regular,
       description: image.description,
@@ -29,18 +30,18 @@ function App() {
 
   const closeModal = () => {
     setIsOpen(false);
-    setSelectedImg("");
+    setSelectedImg(null);
   };
 
   //State for searchBar
-  const [searchTopic, setSearchTopic] = useState("");
+  const [searchTopic, setSearchTopic] = useState<string>("");
   //State for ImageGallery
-  const [images, setImages] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
-  const [hasMoreImages, setHasMoreImages] = useState(false);
+  const [images, setImages] = useState<ImageInfo[] | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [hasMoreImages, setHasMoreImages] = useState<boolean>(false);
   // Value for URL response
   const params = {
     client_id: "KTgPCjIl5bHSKS3rJgiOFvqns88NWiKXgvLhb7v-WzM",
@@ -49,10 +50,12 @@ function App() {
     query: searchTopic,
   };
   // Function for submit
-  const handleSearchSubmit = (event) => {
+  const handleSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const form = event.currentTarget;
-    const searchValue = form.elements.searchInput.value.trim();
+    const searchValue = (
+      form.elements.namedItem("searchInput") as HTMLInputElement
+    ).value.trim();
     if (searchValue === "") {
       toast.error("Поле не заповнене.");
       return;
@@ -63,7 +66,7 @@ function App() {
     form.reset();
   };
   //Add page to before page
-  const handleOnClick = () => {
+  const handleOnClick = (): void => {
     if (hasMoreImages) {
       setPage((prevPage) => prevPage + 1);
     }
@@ -74,10 +77,10 @@ function App() {
       setIsLoading(true);
       setIsError(false);
       try {
-        const { data } = await axios.get(
+        const { data } = await axios.get<ApiResponse>(
           `https://api.unsplash.com/search/photos/?client_id=${params.client_id}&orientation=${params.orientation}&page=${page}&per_page=${params.per_page}&query=${params.query}`
         );
-        setImages((prevImages) => [...prevImages, ...data.results]);
+        setImages((prevImages) => [...(prevImages ?? []), ...data.results]);
         const totalPagesCalc = Math.ceil(data.total / params.per_page);
         setTotalPages(totalPagesCalc);
         setHasMoreImages(page < totalPagesCalc);
@@ -108,6 +111,6 @@ function App() {
       {!isLoading && hasMoreImages && <LoadMoreBtn onClick={handleOnClick} />}
     </>
   );
-}
+};
 
 export default App;
